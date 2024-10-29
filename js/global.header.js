1b2e7087059ecf8b5c6e6a997e5f83efc2d01375
Oleksandra <ssasha.yurchenko@gmail.com>
2024-10-29 20:35:49 +0200

    Fix nav menu, add one more content-block to index.features.html

--- js/global.header.js
+++ js/global.header.js
@@ -1,8 +1,17 @@
-const menuBtn = document.querySelector('.menu__btn');
-const headerNav = document.querySelector('.header__nav');
+const menuBtn = document.querySelector(".menu__btn");
+const headerNav = document.querySelector(".header__nav");
+const navLinks = document.querySelectorAll(".header__nav-list a");
 
-menuBtn.addEventListener('click', () => {
-  headerNav.classList.toggle('open');
-      document.documentElement.classList.toggle("no-scroll");
-    document.body.classList.toggle("no-scroll");
+menuBtn.addEventListener("click", () => {
+  headerNav.classList.toggle("open");
+  document.documentElement.classList.toggle("no-scroll");
+  document.body.classList.toggle("no-scroll");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    headerNav.classList.remove("open");
+    document.documentElement.classList.remove("no-scroll");
+    document.body.classList.remove("no-scroll");
+  });
 });
